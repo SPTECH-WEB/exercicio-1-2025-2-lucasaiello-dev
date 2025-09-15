59f901b0a23b571cfaf29dd24bb8b4e643f38505
lucasaiello-dev <lucas.aiello@sptech.school>
2025-09-14 22:56:59 -0300

    feat(exercicio-01): finish the exercise with success in all tests

--- src/main/java/school/sptech/prova_ac1/UsuarioController.java
+++ src/main/java/school/sptech/prova_ac1/UsuarioController.java
@@ -1,36 +1,81 @@
 package school.sptech.prova_ac1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
     public ResponseEntity<List<Usuario>> buscarTodos() {
-        return ResponseEntity.internalServerError().build();
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        if (usuarios.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        } else {
+            return ResponseEntity.ok(usuarios);
+        }
     }
 
-    public ResponseEntity<Usuario> criar(Usuario usuario) {
-        return ResponseEntity.internalServerError().build();
+    @PostMapping
+    public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario) {
+        if (usuarioRepository.findByEmailOrCpf(usuario.getEmail(), usuario.getCpf()).isEmpty()) {
+            return ResponseEntity.status(201).body(usuarioRepository.save(usuario));
+        } else {
+            return ResponseEntity.status(409).build();
+        }
     }
 
-    public ResponseEntity<Usuario> buscarPorId(Integer id) {
-        return ResponseEntity.internalServerError().build();
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Integer id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if (usuario.isEmpty()) {
+            return ResponseEntity.status(404).build();
+        } else {
+            return ResponseEntity.ok(usuario.get());
+        }
     }
 
-    public ResponseEntity<Void> deletar(Integer id) {
-        return ResponseEntity.internalServerError().build();
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+        if (usuarioRepository.existsById(id)) {
+            usuarioRepository.deleteById(id);
+            return ResponseEntity.status(204).build();
+        } else {
+            return ResponseEntity.status(404).build();
+        }
     }
 
-    public ResponseEntity<List<Usuario>> buscarPorDataNascimento(LocalDate nascimento) {
-        return ResponseEntity.internalServerError().build();
+    @GetMapping("/filtro-data")
+    public ResponseEntity<List<Usuario>> buscarPorDataNascimento(@RequestParam LocalDate nascimento) {
+        List<Usuario> usuarios = usuarioRepository.findByDataNascimentoGreaterThan(nascimento);
+        if (usuarios.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        } else {
+            return ResponseEntity.ok(usuarios);
+        }
     }
 
+    @PutMapping("/{id}")
     public ResponseEntity<Usuario> atualizar(
-            Integer id,
-            Usuario usuario
+            @PathVariable Integer id,
+            @RequestBody Usuario usuario
     ) {
-        return ResponseEntity.internalServerError().build();
+        Optional<Usuario> usuarioSearch = usuarioRepository.findById(id);
+        if (usuarioSearch.isEmpty()) {
+            return ResponseEntity.status(404).build();
+        } else if(usuarioRepository.findByEmailOrCpf(usuario.getEmail(), usuario.getCpf()).size() >= 2) {
+            return ResponseEntity.status(409).build();
+        } else {
+            usuario.setId(id);
+            return ResponseEntity.ok(usuarioRepository.save(usuario));
+        }
     }
 }
--- src/main/java/school/sptech/prova_ac1/UsuarioRepository.java
+++ src/main/java/school/sptech/prova_ac1/UsuarioRepository.java
@@ -1,5 +1,13 @@
 package school.sptech.prova_ac1;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    List<Usuario> findByEmailOrCpf(String email, String cpf);
+    List<Usuario> findByDataNascimentoGreaterThan(LocalDate dataNascimento);
 }
